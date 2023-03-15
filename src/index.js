34e87ed5f549272c95b02d6bf35519a6c2a2f3a0
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2023-03-15 22:28:37 +0200

    зробив розмітку,але не виходить відмалювати

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
+import { fetchCountries } from './js/fetchCountries.js';
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
+refs.input.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
+
+function onFormInput(event) {
+  resetInput();
+  event.preventDefault();
+
+  const form = event.target;
+  const name = form.value.trim();
+  if (name === '') {
+    return;
+  }
+  fetchCountries(name)
+    .then(response => {
+      if (response.length === 1) {
+        oneCountry;
+        console.log(1);
+      } else if (response.length > 1 && response.length <= 10) {
+        listCountries;
+        console.log(2);
+      } else {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+    })
+    .catch(error => {
+      console.log(3);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function resetInput() {
+  refs.info.innerHTML = '';
+  refs.list.innerHTML = '';
+}
+
+function oneCountry(response) {
+  const fullInfo = response
+    .map(({ name, flags, capital, population, languages }) => {
+      return `<img
+      src="${flags.svg}"
+      alt="${name.official}" width = "25" height = "15" />
+      <h1>${name.official}</h1>
+          <p>Capital: ${capital}</p>
+          <p>Population: ${population}</p>
+          <p>Languages: ${languages.map(el => el.value).join(', ')}</p>`;
+    })
+    .join('');
+  console.log(fullInfo);
+  refs.info.innerHTML = fullInfo;
+  refs.list.innerHTML = '';
+}
+
+function listCountries(response) {
+  const shortInfo = response
+    .map(({ name, flags }) => {
+      return `<li>
+    <img src="${flags.svg}"
+    alt="${name.official}"
+    width = "25"
+    height = "15" />
+  <p>${name.official}</p>
+</li>`;
+    })
+    .join('');
+  console.log(shortInfo);
+  refs.list.innerHTML = shortInfo;
+  refs.info.innerHTML = '';
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,9 @@
+export { fetchCountries };
+const BASE_URL = 'https://restcountries.com/v3.1';
+function fetchCountries(name) {
+  return fetch(
+    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    return response.json();
+  });
+}
